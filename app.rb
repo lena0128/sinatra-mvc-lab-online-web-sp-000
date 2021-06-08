0ad03d5523b75e52342db628b9f310eb0588cf23
Lena Hou <lenahou2013@gmail.com>
2021-06-08 14:20:34 -0400

    commit tree 0394ca9c4d2086408880c7e32ebcb34abfa3f488

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        new_phrase = PigLatinizer.new
+        @piglatin_word = new_phrase.piglatinize(params[:user_phrase])
+
+        erb :piglatinize
+    end
+
 end
